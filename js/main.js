9e3cf7aa57b6b317a085715b7f30a344f8b887ce
Richard Marquez <rmarquez14@winona.edu>
2016-01-25 19:14:04 -0600

    refactored; fixed analog poll output

--- js/main.js
+++ js/main.js
@@ -1,54 +1,51 @@
-// ANGULAR
+// INIT
 var app = angular.module('mainApp', ['ngRoute']);
+var socket = io.connect('http://localhost:8080');
 
-var boardTypes = {
-  'uno': {
-    'name': 'UNO',
-    'digitalPins': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
-    'analogPins': [14, 15, 16, 17, 18, 19],
-    'pwmPins': [3, 5, 6, 9, 10, 11]
-  },
 
-  /*'mega': {
-    'name': 'Mega 2560',
-    'digitalPins': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
-    'analogPins': [14, 15, 16, 17, 18, 19]
-  },*/
-  
-  'zero': {
-    'name': 'ZERO',
-    'digitalPins': [0, 1, 2],
-    'analogPins': [3, 4],
-    'pwmPins': [0]
-  }
-};
-
+// BOARD CONFIGURATIONS
 
 app.controller('mainController', function($scope) {
-  $scope.boardTypes = boardTypes;
+  $scope.boardTypes = {
+    'uno': {
+      'name': 'UNO',
+      'digitalPins': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+      'analogPins': [14, 15, 16, 17, 18, 19],
+      'pwmPins': [3, 5, 6, 9, 10, 11]
+    },
 
-  $scope.digitalPins = boardTypes.uno.digitalPins;
-  $scope.analogPins = boardTypes.uno.analogPins;
-  $scope.pwmPins = boardTypes.uno.pwmPins;
+    /*'mega': {
+      'name': 'Mega 2560',
+      'digitalPins': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+      'analogPins': [14, 15, 16, 17, 18, 19]
+    },*/
+    
+    'zero': {
+      'name': 'ZERO',
+      'digitalPins': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+      'analogPins': [14, 15, 16, 17, 18, 19],
+      'pwmPins': [3, 5, 6, 9, 10, 11]
+    }
+  };
+
+  // Set board type to uno initially
+  $scope.board = $scope.boardTypes.uno;
 
   $scope.boardChanged = function() {
     var newBoardType = $('#boardType').val();
 
-    $scope.digitalPins = boardTypes[newBoardType].digitalPins;
-    $scope.analogPins = boardTypes[newBoardType].analogPins;
-    $scope.pwmPins = boardTypes[newBoardType].pwmPins;
+    $scope.board = $scope.boardTypes[newBoardType];
+    initButtons();
   };
 
   $scope.isPWM = function(pin) {
-    return ($.inArray(pin, $scope.pwmPins) > -1 ? true : false);
+    return ($.inArray(pin, $scope.board.pwmPins) > -1 ? true : false);
   };
 
 });
 
 
-var socket = io.connect('http://localhost:8080');
-
-// QUERY HANDLERS
+// POLL MANAGEMENT
 var pinPolls = {};
 
 function stopPoll(pin) {
@@ -67,12 +64,24 @@ function findPinNum(el) {
   return Number(el.find('meta[name="pinNum"]').first().attr('content'));
 }
 
-function findPinValue(el) {
+function findByPinNum(n) {
+  var el = {};
+
+  $('.pin').each(function() {
+    if (n == Number($(this).find('meta[name="pinNum"]').first().attr('content'))) {
+      el = $(this);
+    }
+  });
+
+  return el;
+}
+
+function findPinPWMValue(el) {
   while (!el.hasClass('pin')) {
     el = el.parent();
   }
 
-  return Number(el.find('.valueText').first().val());
+  return Number(el.find('.pwmValue').first().val());
 }
 
 function findPollValue(el) {
@@ -85,7 +94,10 @@ function findPollValue(el) {
 
 
 // INIT
-$(document).ready(function() {
+function initButtons() {
+  // Remove all previous handlers to avoid duplicates
+  $('button').off('click');
+
 
   // POLLS
   $('.stopPollButton').on('click', function() {
@@ -108,19 +120,7 @@ $(document).ready(function() {
     socket.emit('queryState', findPinNum($(this)));
   });
 
-  socket.on('queriedState', function(data) {
-    console.log(data);
-
-    // Add analog queries to textarea
-    var el = $('#' + data.pin);
-    if (el.hasClass('analog')) {
-      var textarea = el.find('textarea').first();
-      textarea.append(data.value + '\r');
-      textarea.animate({scrollTop:textarea[0].scrollHeight - textarea.height()},10);
-    }
-  });
-
-
+  
   // DIGITAL PINS
   $('.digitalModeToggleButton').on('click', function() {
     socket.emit('toggleDigitalMode', findPinNum($(this)));
@@ -138,7 +138,7 @@ $(document).ready(function() {
 
   $('.pwmValueSetButton').on('click', function() {
     var pin = findPinNum($(this));
-    var value = findPinValue($(this));
+    var value = findPinPWMValue($(this));
 
     socket.emit('setPWMValue', { pin: pin, value: value });
   });
@@ -148,5 +148,25 @@ $(document).ready(function() {
   $('.analogModeToggleButton').on('click', function() {
     socket.emit('toggleAnalogMode', findPinNum($(this)));
   });
+}
 
+function initSocket() {
+  socket.on('queriedState', function(data) {
+    console.log(data);
+
+    // Add analog queries to textarea
+    var el = findByPinNum(data.pin);
+    if (el.hasClass('analog')) {
+      var textarea = el.find('textarea').first();
+      textarea.append(data.value + '\r');
+      textarea.animate({scrollTop:textarea[0].scrollHeight - textarea.height()},10);
+    }
+  });
+}
+
+
+$(document).ready(function() {
+  initButtons();
+  initSocket();
 });
+
